8e90242b5bf3be68f1818f2efac0dbe6f4dff4c2
Bryce Van Camp <brycecam@amazon.com>
2020-07-21 16:09:25 -0500

    Forgot to save cli.py

--- ssm_acquire/cli.py
+++ ssm_acquire/cli.py
@@ -114,9 +114,11 @@ def _main_helper(instance_id, region, build, acquire, interrogate, analyze):
 
     ssm_client = _get_ssm_client(credentials, region)
 
+    # TESTING
     ensure_command(ssm_client, ['cat /etc/os-release'], instance_id)
 
     exit()
+    # TESTING
 
     _resolve_flags(
         analyze, 
